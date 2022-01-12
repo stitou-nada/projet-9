241554a6d7f1322cc8ebcedc6cde24b215ac1711
stitou-nada <nadastito1087@gmail.com>
2022-01-12 16:52:54 +0000

    add

---
+++ prototype/scripts.js
@@ -0,0 +1,33 @@
+// const btn = document.querySelector('#btn');
+// const sb = document.querySelector('#framework')
+// btn.onclick = (event) => {
+//     event.preventDefault();
+
+//     sb = city
+//  };
+
+
+
+var  city = prompt();
+var url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0bd0cd1e7d8ab7a578a5a4d28a57d45b`
+
+
+
+fetch(url)
+.then(function(reponse){
+    return reponse.json()
+
+})
+.then(function(data){
+
+    var city = data.name
+
+    var temprture = data.main.temp
+    var sky = data.weather[0].description
+
+
+    // document.write(city + "<br>"+ sky+"<br>"+ temprture)
+
+console.log(data)
+
+})

5c22ae806f01febe1cc7fbfdbc44972e49dc52ee
stitou-nada <nadastito1087@gmail.com>
2022-01-19 16:17:21 +0000

    add

--- realisation/main.js
+++ realisation/main.js
@@ -1,24 +1,44 @@
-var city = prompt()
+var city;
 
-var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0bd0cd1e7d8ab7a578a5a4d28a57d45b`
+var key = '1253309e30b4fb953c136c1426565be0&units=metric';
+
+async function onSearch() {
+    var weatherManager = new WeatherManager()
+
+    city = document.getElementById("cityInput").value
+    var url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
+
+    if (city) {
+        weatherManager.fetchForecast(url).then(() => {
+            display(weatherManager.currentForecast)
+        })
+    }
+
+    document.getElementById("cityInput").value = ""
+    city = undefined
+
+  
 
 
-
-fetch(url)
-    .then(function (reponse) {
-        return reponse.json()
-
-    })
-    .then(function (data) {
-
-        var city = ("City :   " + data.name)
-
-        var wind = ("Wind speed:  " + data.wind.speed)
-        var sky = ('Weather:   ' + data.weather[0].description)
+}
 
 
-        document.write(city + "<br>" + sky + "<br>" + wind)
+function display(currentForecast) {
+    document.getElementById('city').textContent= currentForecast.city
+    document.getElementById('temp').textContent= currentForecast.temp+ ""+ "°C"
 
-    console.log(data)
 
-    })
+    document.getElementById('desc').textContent= currentForecast.description
+    document.getElementById('country').textContent= currentForecast.humidity
+
+}
+
+    let dateToday =document.getElementById("date");
+  let today = new Date();
+  let day =`${today.getDate() < 10?"0" :""}${today.getDate()}`
+    let month =`${(today.getMonth()+1) < 10?"0" :""}${today.getMonth()+1}`
+
+    let year =today.getFullYear();
+
+    dateToday.textContent =`${day}/${month}/${year}`
+    
---
+++ realisation/weatherManager.js
@@ -0,0 +1,53 @@
+class WeatherManager {
+    #_currentForecast;
+    #_weekForecast;
+
+    set currentForecast(value) {
+        this.#_currentForecast = value
+    }
+
+    get currentForecast() {
+        return this.#_currentForecast
+    }
+
+    // async fetchForecast(url) {
+    //     var weather = new Weather()
+
+    //     fetch(url)
+    //     .then(response => {
+    //         // handle the response
+    //         return response.json()
+    //     }).then(json => {
+    //         weather.city = json.name
+    //         weather.description = json.weather[0].main
+    //         weather.iconCode = json.weather[0].icon
+    //         weather.temp = json.main.temp
+    //         weather.humidity = json.main.humidity
+    //         this.#_currentForecast = weather
+    //         // console.log(this.#_currentForecast)
+    //     })
+    //     .catch(error => {
+    //         // handle the error
+    //         console.log(error)
+    //     });
+    //     return await weather
+
+    // }
+
+    async fetchForecast(url) {
+
+        var response = await fetch(url)
+        var json = await response.json()
+        // handle the response
+
+        var weather = new Weather()
+        weather.city = json.name
+        weather.description = json.weather[0].main
+        weather.iconCode = json.weather[0].icon
+        weather.temp = json.main.temp
+        weather.country = json.main.country
+        weather.humidity = json.main.humidity
+        this.#_currentForecast = weather
+
+    }
+}

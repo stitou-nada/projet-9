e61787ba6e9320fe4acd04529a9b4592f9dd756e
stitou-nada <nadastito1087@gmail.com>
2022-01-21 10:39:15 +0000

    realisation

--- realisation/main.js
+++ realisation/main.js
@@ -1,36 +1,37 @@
-var city;
 
-var key = '1253309e30b4fb953c136c1426565be0&units=metric';
+    var city;
 
-async function onSearch() {
-    var weatherManager = new WeatherManager()
+    var key = '1253309e30b4fb953c136c1426565be0&units=metric';
 
-    city = document.getElementById("cityInput").value
-    var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0bd0cd1e7d8ab7a578a5a4d28a57d45b&units=metric`;
+    async function onSearch() {
+        var weatherManager = new WeatherManager()
 
-    if (city) {
-        weatherManager.fetchForecast(url).then(() => {
-            display(weatherManager.currentForecast)
-        })
+        city = document.getElementById("cityInput").value
+        var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0bd0cd1e7d8ab7a578a5a4d28a57d45b&units=metric`;
+
+        if (city) {
+            weatherManager.fetchForecast(url).then(() => {
+                display(weatherManager.currentForecast)
+            })
+        }
+
+        document.getElementById("cityInput").value = ""
+        city = undefined
+
+
+
+
     }
 
-    document.getElementById("cityInput").value = ""
-    city = undefined
+    
+    function display(currentForecast) {
+        document.getElementById('city').textContent = currentForecast.city
+        document.getElementById('temp').textContent = currentForecast.temp + "" + "°C"
 
+        document.getElementById('icon').src = "http://openweathermap.org/img/w/" + currentForecast.iconCode + ".png";
+        document.getElementById('desc').textContent = currentForecast.description
+        document.getElementById('country').textContent = currentForecast.humidity
+    
+   
 
-
-
-}
-
-
-function display(currentForecast) {
-    document.getElementById('city').textContent = currentForecast.city
-    document.getElementById('temp').textContent = currentForecast.temp + "" + "°C"
-
-    document.getElementById('icon').src = "http://openweathermap.org/img/w/"+currentForecast.iconCode+".png";
-    document.getElementById('desc').textContent = currentForecast.description
-    document.getElementById('country').textContent = currentForecast.humidity
-
-
-
-}
+    }
